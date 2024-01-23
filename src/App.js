deb512d176ed5d10010002df4027636c1d220cab
meetravi0706 <raviprasad2490@gmail.com>
2024-01-23 12:01:16 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Homepage } from "./pages/Homepage";
+import { ChatPage } from "./pages/ChatPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route exact path="/" Component={Homepage} />
+        <Route exact path="/chats" Component={ChatPage} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect } from "react";
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import { Login } from "../components/Authentication/Login";
+import { Signup } from "../components/Authentication/Signup";
+import { useNavigate } from "react-router-dom";
+
+export const Homepage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+
+    if (user) navigate("/chats");
+  }, [navigate]);
+
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        display={"flex"}
+        justifyContent={"center"}
+        p={3}
+        bg={"white"}
+        width="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work sans" color={"black"}>
+          Talk-A-Tive
+        </Text>
+      </Box>
+      <Box
+        bg={"white"}
+        width="100%"
+        p={4}
+        borderRadius="lg"
+        borderWidth="1px"
+        color={"black"}
+      >
+        <Tabs variant="soft-rounded">
+          <TabList mb={"1em"}>
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
